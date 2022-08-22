6894d04f07efb729d78aa0ab82fa8a475c6f5b0f
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-08-22 23:01:56 +0500

    commit

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-const age = prompt("Enter your age");
-Array.from(document.querySelectorAll(".dd")).map((e, _) => _ === 2 || _ === 5 && (e.innerText = age));
+const age = prompt("yo");
+const date = prompt("date");
+
+setTimeout(() => { 
+    Array.from(document.querySelectorAll(".dd")).map((e, _) => {
+        _ === 2 && (e.innerText = age.toString()); 
+        if (_ === 5) {
+            e.innerText = e.textContent.split(" ").slice([content.length - 1]).push(date).join();
+        };
+    }); 
+}, 0) 
