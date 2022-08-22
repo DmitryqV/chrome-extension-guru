9ac0ddb9c8fc033fae404231b7e48d6e8e9a3e16
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-08-22 23:45:51 +0500

    commit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ setTimeout(() => {
     Array.from(document.querySelectorAll(".dd")).map((e, _) => {
         _ === 2 && (e.innerText = age.toString()); 
         if (_ === 5) {
-            e.innerText = e.textContent.split(" ").slice([content.length - 1]).push(date).join();
+            e.textContent = `${e.textContent.substring(0, e.textContent.trim().lastIndexOf(" "))}и ${date}`
         };
     }); 
 }, 0) 
