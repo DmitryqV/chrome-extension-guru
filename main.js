947d07b6c0b25d94a12c4ae4170d880e1e34a0ef
Dmitry Vlasenko <67154333+DmitryqV@users.noreply.github.com>
2022-08-23 15:03:48 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
-const age = "";
-const date = "";
+/**
+* Example
+* const [releaseDate, birthDate] = ["24.11.2015", "11.11.2001"];
+*/
+const [releaseDate, birthDate] = ["", ""];
 
+/**
+* Execute code after all requests
+*/
 setTimeout(() => {
+  /**
+  * Running code after rendering
+  * Max rate = 200ms
+  */
   setTimeout(() => {
+    /**
+    * Work with dom nodes
+    */
     Array.from(document.querySelectorAll(".dd")).map((e, _) => {
-      if (_ === 2) {
-        e.innerText = age.toString();
-      }
+      if (_ === 2) e.innerText = birthDate;
       
       if (_ === 5) {
-        e.innerText = `${e.textContent.substring( 0, e.textContent.trim().lastIndexOf(" "))}и ${date}`;
+        e.innerText = `${e.textContent.substring(0, e.textContent.trim().lastIndexOf(" "))}и ${releaseDate}`;
       }
-
     });
+    /**
+    * Coming soon
+    */
   }, 200);
 }, 0);
